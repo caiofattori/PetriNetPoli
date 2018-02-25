c31cc105ce224d7782714ba819d8d9fea0b2a2e9
Caio Fattori <caio@localhost.localdomain>
2018-02-25 13:15:05 -0300

    Criando interface via UI designer

--- src/main/cpp/petrinetwindow.cpp
+++ src/main/cpp/petrinetwindow.cpp
@@ -1,16 +1,38 @@
-#include <iostream>
+/**
+*
+*   PetriNetPoli is a GUI for modeling, analysing and simulating Petri net
+*   Copyright (C) 2017  Caio C Fattori
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+*/
+
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QVBoxLayout>
+#include <QFile>
+#include <QFormBuilder>
 
 #include "petrinet.h"
 
 
 PetriNetWindow::PetriNetWindow()
 {
-	QWidget* wg = new QWidget;
-	setCentralWidget(wg);
-	QVBoxLayout* main_layout = new QVBoxLayout;
-	wg->setLayout(main_layout);
+    QFormBuilder loader;
+    QFile form(":/src/main/resource/petrinetwindow.ui");
+    form.open(QFile::ReadOnly);
+    QWidget* wg = loader.load(&form, this);
+    form.close();
 }
 
 
